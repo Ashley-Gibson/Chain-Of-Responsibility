e00763b10bfe46d9e542f662592f252abafa34ff
ashleydgibson <ashley_gibson@ajg.com>
2020-03-12 14:09:46 +0000

    task/COR-100 - Initial commit for Program.cs

--- Chain of Responsibility/Chain of Responsibility/Program.cs
+++ Chain of Responsibility/Chain of Responsibility/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Chain_of_Responsibility
+﻿using System;
+
+namespace Chain_of_Responsibility
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Chain of Responsibility *****");
+            Console.WriteLine("\n");
+            Console.WriteLine("***** Link 1 - Authentication *****");
+            Console.WriteLine("\n");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("***** Link 2 - Authorization *****");
+            Console.WriteLine("\n");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("***** Link 3 - Validation *****");
+            Console.WriteLine("\n");
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
         }
     }
 }
