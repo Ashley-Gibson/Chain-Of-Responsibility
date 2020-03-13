0fbf46bf8d1b7560f6528a08428d124b2fe27f9e
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-03-13 19:48:36 +0000

    task/COR-109 - Initial commit for Unit Testing suite

---
+++ Chain of Responsibility/Unit Testing/ChainStep_Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Testing
+{
+    [TestClass]
+    public class ChainStep_Tests
+    {
+        [TestMethod]
+        public void UnitOfWork_InitialCondition_ExpectedResult()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
---
+++ Chain of Responsibility/Unit Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a0a4ac1e-5fbf-4636-911d-24aad112d04f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
